b2acdfee2c47c4e57bc69d4461e8d41962598480
victord <victord@SGPL5CG5291KZ8.amd.com>
2016-04-09 15:07:30 +0800

    Tile support

--- src/MatrixMultiply.c
+++ src/MatrixMultiply.c
@@ -12,6 +12,8 @@
 #include <float.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#define min(a,b) fmin(a,b)
 
 int num_gen(int max_size, char *var);
 void array_gen(size_t x, size_t y, float A[x][y]);
@@ -23,42 +25,61 @@ int main()
 {
   int m, n;
   int p, q;
-  int max_size = 10;
+  int max_size;
+  clock_t begin, end;
+  double time_spent;
   int i, j, k;
+  int ii, jj, tile_size;
   float sum = 0.0;
   srand(time(NULL));
+  for (int x = 400; x <= 400; x+=5) {
+    max_size = x;
+    for (int t = 1; t <= 400; t++) {
+      tile_size = t;
 
-  printf("Max float is %f\n",FLT_MAX);
-  printf("Max rand is %d\n",RAND_MAX);
+      // Create matrix A[m][n]
+      //  m = num_gen(max_size,"m");
+      //  n = num_gen(max_size,"n");
+      m = max_size;
+      n=m; q=m;
+      float A[m][n];
+      array_gen(m,n,A);
+      //  array_print(m,n,A);
 
-  // Create matrix A[m][n]
-  m = num_gen(max_size,"m");
-  n = num_gen(max_size,"n");
-  float A[m][n];
-  array_gen(m,n,A);
-  array_print(m,n,A);
+      // Create matrix B[p][q]
+      p = n; // in order for MM to be valid
+      //  q = num_gen(max_size, "q");
+      float B[p][q];
+      array_gen(p,q,B);
+      //  array_print(p,q,B);
 
-  // Create matrix B[p][q]
-  p = n; // in order for MM to be valid
-  q = num_gen(max_size, "q");
-  float B[p][q];
-  array_gen(p,q,B);
-  array_print(p,q,B);
+      begin = clock();
 
-  // Fun with Matrix Multiply
-  // A[m][n] * B[p][q] = X[m][q]
-  float X[m][q];
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < q; j++) {
-      for (k = 0; k < p; k++) {
-        sum = sum + A[i][k]*B[k][j];
+      // Fun with Matrix Multiply
+      // A[m][n] * B[p][q] = X[m][q]
+      float X[m][q];
+
+      for (ii = 0; ii < m; ii+=tile_size) {
+        for (jj = 0; jj < q; jj+=tile_size) {
+          for (i = ii; i < min(ii+tile_size-1,m); i++) {
+            for (j = jj; j < min(jj+tile_size-1,n); j++) {
+              for (k = 0; k < p; k++) {
+                sum = sum + A[i][k]*B[k][j];
+              }
+              X[i][j] = sum;
+              sum = 0.0;
+            }
+          }
+        }
       }
 
-      X[i][j] = sum;
-      sum = 0;
+      end = clock();
+      time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+      printf("Array size = %d, Tile size = %d, Total time = %g\n",max_size, tile_size, time_spent);
+
+      //  array_print(m,q,X);
     }
   }
-  array_print(m,q,X);
 
   return 0;
 }
